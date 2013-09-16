195a602613e2571d7a80d124a64d6d64ee047727
jeukel <jeukel@gmail.com>
2013-09-16 11:38:47 -0600

    Implements Circular Double and Simple List. Pull out NodeList Class. TO DO: TEST.

--- src/edu/itcr/startec/datastructs/doublelist/DoubleList.java
+++ src/edu/itcr/startec/datastructs/doublelist/DoubleList.java
@@ -4,44 +4,6 @@ import java.util.Iterator;
 
 import edu.itcr.startec.datastructs.ListInterface;
 
-class DoubleListNode<K> {
-    
-    protected K elem;
-    protected DoubleListNode<K> next;
-    protected DoubleListNode<K> previous;
-    
-    public DoubleListNode(K elem) {
-        this.elem = elem;
-        this.next = null;
-        this.previous = null;
-    }
-    public DoubleListNode(K elem, DoubleListNode<K> next) {
-        this.elem = elem;
-        this.next = next;
-        this.previous = null;
-    }
-    
-    public void setNext(DoubleListNode<K> next) {
-        this.next = next;
-    }
-    
-    public void setPrevious(DoubleListNode<K> previous) {
-        this.previous = previous;
-    }
-
-    public DoubleListNode<K> getNext() {
-        return this.next;
-    }
-    
-    public DoubleListNode<K> getPrevious() {
-        return this.previous;
-    }
-    
-    public K getElem() {
-        return this.elem;
-    }
-}
-
 class DoubleListIterator<K> implements Iterator<K> {
 
     DoubleList<K> list;
--- src/edu/itcr/startec/datastructs/simplelist/SimpleList.java
+++ src/edu/itcr/startec/datastructs/simplelist/SimpleList.java
@@ -4,33 +4,6 @@ import java.util.Iterator;
 
 import edu.itcr.startec.datastructs.ListInterface;
 
-class SimpleListNode<K> {
-    
-    protected K elem;
-    protected SimpleListNode<K> next;
-    
-    public SimpleListNode(K elem) {
-        this.elem = elem;
-        this.next = null;
-    }
-    public SimpleListNode(K elem, SimpleListNode<K> next) {
-        this.elem = elem;
-        this.next = next;
-    }
-    
-    public void setNext(SimpleListNode<K> next) {
-        this.next = next;
-    }
-
-    public SimpleListNode<K> getNext() {
-        return this.next;
-    }
-    
-    public K getElem() {
-        return this.elem;
-    }
-}
-
 class SimpleListIterator<K> implements Iterator<K> {
 
     SimpleList<K> list;
